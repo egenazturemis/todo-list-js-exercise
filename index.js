0ea2f63d5f1751701413bf04874dc1818b9a66e1
Egenaz <egenazturemis@gmail.com>
2020-12-04 20:06:19 +0000

    fixed a bug in the function

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
 
     logState: function() {
-      console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-    }
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
 
-    markCompleted: function () {
+    markCompleted: function() {
       this.complete = true;
     }
   };
@@ -24,6 +24,7 @@ function newTask(title, description) {
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
+
 
 task1.logState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
